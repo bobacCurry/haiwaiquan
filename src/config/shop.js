b740c8e51bfaab36aad7892a3daffb04bdee0c99
bobacCurry <bobac.curry@gmail.com>
2021-05-29 16:16:04 +0800

    fix

--- src/config/shop.js
+++ src/config/shop.js
@@ -3,8 +3,10 @@ export const SERVICETYPE = [
 	{ name: '生鲜', icon: '/img/home/vegetable.png' },
 	{ name: '超市', icon: '/img/home/cart.png' },
 	{ name: '服饰', icon: '/img/home/coat.png' },
+	{ name: '美妆', icon: '/img/home/cosmetics.png' },
 	{ name: '药品', icon: '/img/home/pharmacy.png' },
-	{ name: '换汇', icon: '/img/home/bank.png' }
+	{ name: '换汇', icon: '/img/home/bank.png' },
+	{ name: '娱乐', icon: '/img/home/ktv.png' },
 ]
 
 export const PAYMENT = [ '现金到付', '线上付款' ]
@@ -13,4 +15,4 @@ export const DELIVERY = [ '商家配送', '第三方配送' ]
 
 export const CURRENCY = [ 'P', 'CNY', 'USD', 'AED' ]
 
-export const UNIT = ['份','个','串','只','瓶','把','g','kg','斤','盒','包','粒','支','袋','组','例']
+export const UNIT = ['份','个','串','只','瓶','把','g','kg','斤','盒','包','粒','支','袋','组','例','件','双','条']
